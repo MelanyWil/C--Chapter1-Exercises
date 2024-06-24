dc93b54a10356a04c169917eb33414ba9340c5c0
Melany Willis <melexiw@gmail.com>
2024-06-24 13:06:55 -0500

    More Numeric Types

---
+++ MoreNumericTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("How many miles have you driven? ");
+int miles = int.Parse(Console.ReadLine());
+Console.WriteLine("How many gallons of gas have you used? ");
+int gas = int.Parse(Console.ReadLine());
+int mpg = miles / gas;
+Console.WriteLine("Your mpg for this trip was " + mpg);
