492b61bcfac7ea8796f7e0dd53688eaad135aa16
Melany Willis <melexiw@gmail.com>
2024-06-24 12:54:55 -0500

    numberic types

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is the length of your rectangle?");
+double l = double.Parse(Console.ReadLine());
+Console.WriteLine("What is the width of your rectangle?");
+double w = double.Parse(Console.ReadLine());
+double area = l * w;
+Console.WriteLine("The area of the rectangle is " + area);
